59d690b55298e54db2ceac035bcd643de70e1b5a
Muhamad yazid zinky arisona <144634381+yazidzky@users.noreply.github.com>
2025-07-06 11:52:13 +0700

    Delete todolist directory

--- todolist/src/main/java/com/example/todolist/exception/TaskNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example.todolist.exception;
-
-public class TaskNotFoundException extends RuntimeException {
-    public TaskNotFoundException(Long id) {
-        super("Task not found with id: " + id);
-    }
-}
